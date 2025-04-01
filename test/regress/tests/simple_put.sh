18a6cc56599169163e13033d7fae49aff126a0f2
Yury Frolov <yura200253@gmail.com>
2025-04-01 19:19:57 +0500

    WIP break down to smaller tests

---
+++ test/regress/tests/simple_put.sh
@@ -0,0 +1,3 @@
+echo 'Sample data' | yp-client --config test/regress/yproxy.yaml -l fatal put 'some_file'
+yp-client --config test/regress/yproxy.yaml -l fatal list ''
+yp-client --config test/regress/yproxy.yaml -l fatal cat 'some_file'
