e751909cb65188f6585f396c5b4a104440d31770
Yury Frolov <yura200253@gmail.com>
2025-04-02 12:34:07 +0500

    Split to multiple tests

---
+++ test/regress/tests/encrypted_put.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+
+echo 'Sample encrypted data' | yp-client --config test/regress/yproxy.yaml -l fatal put --encrypt 'encrypted_file'
+yp-client --config test/regress/yproxy.yaml -l fatal list ''
+yp-client --config test/regress/yproxy.yaml -l fatal cat --decrypt 'encrypted_file'
