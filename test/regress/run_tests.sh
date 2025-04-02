365ffbf841056f82fdef98710261d1ea5d464563
Yury Frolov <yura200253@gmail.com>
2025-04-02 13:24:19 +0500

    Add test for copy with dest storage prefix

--- test/regress/run_tests.sh
+++ test/regress/run_tests.sh
@@ -2,13 +2,13 @@
 set -ex
 
 yproxy --config test/regress/conf/yproxy.yaml --test-mode &
+yproxy --config test/regress/conf/yproxy_prexix.yaml --test-mode &
 yproxy --config test/regress/conf/yproxy_old.yaml --test-mode &
 yproxy --config test/regress/conf/yproxy_second_bucket.yaml --test-mode &
 
 # Wait for yproxy to become available
 i=0
-while (! [ -S /tmp/yproxy.sock ] || ! [ -S /tmp/yproxy_old.sock ] || ! [ -S /tmp/yproxy_identical.sock ] ) && [ $i -lt 20 ]; do sleep 1; i=$(($i+1)) ; done
-( [ -S /tmp/yproxy.sock ] && [ -S /tmp/yproxy_old.sock ] || ! [ -S /tmp/yproxy_identical.sock ] ) || ( echo 'yproxy startup failed'; exit 2 )
+while (! [ -S /tmp/yproxy.sock ] || ! [ -S /tmp/yproxy_old.sock ] || ! [ -S /tmp/yproxy_identical.sock ] || ! [ -S /tmp/yproxy_prefix.sock ] ) && [ $i -lt 20 ]; do sleep 1; i=$(($i+1)) ; done
 
 for test in $(ls test/regress/tests | awk '{print(substr($1, 1, length($1)-3))}' )
     do ./test/regress/tests/${test}.sh > output.txt || true
