57baa2814a26cbdece97c0e42d28b0356b193460
reshke <reshke@double.cloud>
2026-05-08 12:27:09 +0500

    Rate limit yezzey vacuum utilities with files-per-second (#166)
    * Rate limit yezzey vacuum utilities with files-per-second
    
    Reviewed-by: xifos@yandex-team.ru

--- config/vacuum.go
+++ config/vacuum.go
@@ -1,5 +1,6 @@
 package config
 
 type Vacuum struct {
-	CheckBackup bool `json:"check_backup" toml:"check_backup" yaml:"check_backup"`
+	CheckBackup     bool `json:"check_backup" toml:"check_backup" yaml:"check_backup"`
+	FileChunkPerSec int  `json:"file_chunk_per_sec" toml:"file_chunk_per_sec" yaml:"file_chunk_per_sec"`
 }
