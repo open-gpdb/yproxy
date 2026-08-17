eb791c6fa0ee6fe029423d3da12a1c6cd8eb1e3f
reshke <reshke@double.cloud>
2026-08-17 15:19:22 +0500

    Basic CI for plain delete (trash move) (#213)
    * Basic CI for plain delete (trash move)

--- test/regress/run_tests.sh
+++ test/regress/run_tests.sh
@@ -5,11 +5,12 @@ yproxy --config test/regress/conf/yproxy.yaml --test-mode &> /var/log/yproxy.log
 yproxy --config test/regress/conf/yproxy_prefix.yaml --test-mode &> /var/log/yproxy_prefix.log &
 yproxy --config test/regress/conf/yproxy_old.yaml --test-mode &> /var/log/yproxy_old.log &
 yproxy --config test/regress/conf/yproxy_second_bucket.yaml --test-mode &> /var/log/yproxy_second_bucket.log &
+yproxy --config test/regress/conf/yproxy_vacuum.yaml --test-mode &> /var/log/yproxy_vacuum.log &
 
 # Wait for yproxy to become available
 i=0
-while (! [ -S /tmp/yproxy.sock ] || ! [ -S /tmp/yproxy_old.sock ] || ! [ -S /tmp/yproxy_identical.sock ] || ! [ -S /tmp/yproxy_prefix.sock ] ) && [ $i -lt 20 ]; do sleep 1; i=$(($i+1)) ; done
-( [ -S /tmp/yproxy.sock ] && [ -S /tmp/yproxy_old.sock ] && [ -S /tmp/yproxy_identical.sock ] && [ -S /tmp/yproxy_prefix.sock ] ) || ( echo 'yproxy startup failed'; exit 2 )
+while (! [ -S /tmp/yproxy.sock ] || ! [ -S /tmp/yproxy_old.sock ] || ! [ -S /tmp/yproxy_identical.sock ] || ! [ -S /tmp/yproxy_prefix.sock ] || ! [ -S /tmp/yproxy_vacuum.sock ] ) && [ $i -lt 20 ]; do sleep 1; i=$(($i+1)) ; done
+( [ -S /tmp/yproxy.sock ] && [ -S /tmp/yproxy_old.sock ] && [ -S /tmp/yproxy_identical.sock ] && [ -S /tmp/yproxy_prefix.sock ] && [ -S /tmp/yproxy_vacuum.sock ] ) || ( echo 'yproxy startup failed'; exit 2 )
 
 for test in $(ls test/regress/tests | awk '{print(substr($1, 1, length($1)-3))}' )
     do ./test/regress/tests/${test}.sh > output.txt || true
@@ -18,5 +19,6 @@ for test in $(ls test/regress/tests | awk '{print(substr($1, 1, length($1)-3))}'
         do s3cmd --access_key some_key --secret_key some_key --host minio:9000 --host-bucket "" --no-ssl rm $file 
     done
     [ $(s3cmd --access_key some_key --secret_key some_key --host minio:9000 --host-bucket "" --no-ssl la -r | grep "\S" | wc -l ) -eq 0 ] || ( echo s3 not empty; exit 2 )
+    # Ugh. TODO: drop/create extension
     psql -h pg -U postgres -d test -c "DELETE FROM yezzey.yezzey_virtual_index"
 done
---
+++ test/regress/tests/08_delete_trash_dryrun.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -ex
+
+echo 'trash data' | yp-client --config test/regress/conf/yproxy_vacuum.yaml -l fatal put 'trash/garbage_file'
+yp-client --config test/regress/conf/yproxy_vacuum.yaml -l fatal list ''
+yp-client --config test/regress/conf/yproxy_vacuum.yaml -l fatal deleteTrash 'trash'
+yp-client --config test/regress/conf/yproxy_vacuum.yaml -l fatal list ''
